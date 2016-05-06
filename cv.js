45199dd9f30b360ca6ce694b8c579e52d9ad7b7e
Kiet Nguyen <p5ntaboi@gmail.com>
2016-05-06 21:16:52 +0700

    finish

--- cv.js
+++ cv.js
@@ -6,3 +6,19 @@ Cv.controller('TodoCtrl', function($scope, $http) {
           $scope.todos = res.data;                
         });
 });
+function myCtrl($scope) {
+
+  $scope.items = [{
+    name: "Input",
+    editing: false
+  }];
+
+  $scope.editItem = function(item) {
+    item.editing = true;
+  }
+
+  $scope.doneEditing = function(item) {
+    item.editing = false;
+    //dong some background ajax calling for persistence...
+  };
+}
